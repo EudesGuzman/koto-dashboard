3793096f9e6789c605e3139e4fed4b9b4d5db9e5
Eudes Guzmán <eudesgg19@gmail.com>
2020-09-03 09:27:32 +0000

    modify responsive

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,45 +6,17 @@ import logo from "../../img/icon-kotokan.png";
 
 export const Navbar = () => {
 	return (
-		/* 	<nav className="navbar navbar-expand-lg color-principal">
-			<a className="navbar-brand" href="#">
-				<img src={logo} />
-			</a>
-			<p>
-				Kotokan for school <span className="badge badge-secondary">Beta</span>
-			</p>
-
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav mr-auto">
-					<li className="nav-item active">
-						<a className="nav-link" href="#">
-							Home <span className="sr-only">(current)</span>
-						</a>
-					</li>
-				</ul>
-				<form className="form-inline my-2 my-lg-0">
-					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link" href="#">
-								Log out <ExitToAppIcon />
-							</a>
-						</li>
-					</ul>
-				</form>
-			</div>
-        </nav> */
-
 		<div className="navbar">
 			<div className="navbar__left">
 				<Link>
 					<img src={logo} />
 				</Link>
 
-				<p className="navbar__input">Kotokan for school</p>
+				<p className="navbar__input hide-on-mobile ml-2">Kotokan for school</p>
 
-				<span className="badge badge-secondary">Beta</span>
+				<span className="badge badge-secondary ml-2">Beta</span>
 
-				<Link className="navbar__link">Home</Link>
+				<Link className="navbar__link ml-2">Home</Link>
 			</div>
 
 			<div>
--- src/js/layout.js
+++ src/js/layout.js
@@ -8,7 +8,6 @@ import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { SideBar } from "./component/sidebar";
 
 //create your first component
 const Layout = () => {
