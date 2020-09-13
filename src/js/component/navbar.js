c4ba1ee60bf2704c1d4a3d33d08836638f5f6da0
Eudes Guzmán <eudesgg19@gmail.com>
2020-09-13 20:01:26 +0000

    Fix styles and align

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.scss";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+/* import ExitToAppIcon from "@material-ui/icons/ExitToApp";*/
 import logo from "../../img/icon-kotokan.png";
 
 export const Navbar = () => {
@@ -12,16 +12,14 @@ export const Navbar = () => {
 					<img src={logo} />
 				</Link>
 
-				<p className="navbar__input hide-on-mobile ml-2">Kotokan for school</p>
+				<p className="navbar__input hide-on-mobile ml-2 ">Kotokan for school</p>
 
 				<span className="badge badge-secondary ml-2">Beta</span>
 
-				<Link className="navbar__link ml-2">Home</Link>
+				<Link className="navbar__link ml-2 text-dark">Home</Link>
 			</div>
 
-			<div>
-				Log out <ExitToAppIcon />
-			</div>
+			<div>Log out {/* <ExitToAppIcon /> */}</div>
 		</div>
 	);
 };
