70d974445da44475fb4fac52b4b2d82d0739fdb2
davidca12 <ca1david23@gmail.com>
2020-09-23 08:50:31 +0000

    flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,18 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			student: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			loadStudent: async () => {
+				let response = await fetch(
+					"https://3000-de550fc6-f913-4714-9352-246c4c81b707.ws-eu01.gitpod.io/students"
+				);
+				response = await response.json();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+				setStore({
+					student: response
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
 		}
 	};
