e12c31d19efb08042e693571130cfc342b3ddd3d
Eudes Guzmán <eudesgg19@gmail.com>
2020-10-03 12:23:18 +0000

    Add bullet lastProblem

--- src/js/component/Problems.js
+++ src/js/component/Problems.js
@@ -13,11 +13,21 @@ export const Problems = props => {
 		const colors = { "1": "yellow", "2": "green", "3": "red", "4": "blue" };
 
 		if (trueOrFalse) {
-			return <td>negro</td>;
-		} else {
-			/* 	let color = "white";
-			const colors = { "1": "yellow", "2": "green", "3": "red", "4": "blue" }; */
+			Object.keys(difficulty)
+				.sort((a, b) => parseInt(a) - parseInt(b))
+				.map(key => {
+					let value = difficulty[key];
 
+					if (value.completed) {
+						color = colors[key];
+					}
+				});
+			return (
+				<td style={{ "background-color": color }}>
+					<i className="fas fa-circle lastPoint" />
+				</td>
+			);
+		} else {
 			Object.keys(difficulty)
 				.sort((a, b) => parseInt(a) - parseInt(b))
 				.map(key => {
